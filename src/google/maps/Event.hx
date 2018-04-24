194a6902e3de3d3dffc721600696399807403610
Kevin Leung <kevinresol@gmail.com>
2018-04-25 01:31:08 +0800

    Fix events

--- src/google/maps/Event.hx
+++ src/google/maps/Event.hx
@@ -6,11 +6,13 @@ import haxe.Constraints;
 extern class Event {
 	static function addDomListener(target:js.html.Element, type:String, handler:Function):MapEventListener;
 	static function addDomListenerOnce(target:js.html.Element, type:String, handler:Function):MapEventListener;
-	static function addListener(target:Dynamic, event:String, handler:Function):Void;
-	static function addListenerOnce(target:Dynamic, event:String, handler:Function):Void;
+	static function addListener(target:Dynamic, event:String, handler:Function):MapEventListener;
+	static function addListenerOnce(target:Dynamic, event:String, handler:Function):MapEventListener;
 	static function removeListener(listener:MapEventListener):Void;	
 	static function clearListeners(target:Dynamic, event:String):Void;
 	static function clearInstanceListeners(target:Dynamic):Void;
 }
 
-extern class MapEventListener {}
+extern class MapEventListener {
+	function remove():Void;
+}
