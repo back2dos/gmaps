c66bb75f20e809151edc50295b50b57b31387170
Kevin Leung <kevinresol@gmail.com>
2018-07-17 19:23:01 +0800

    More setters for Polygon

--- src/google/maps/Polygon.hx
+++ src/google/maps/Polygon.hx
@@ -4,9 +4,19 @@ import haxe.Constraints;
 import haxe.extern.*;
 import google.maps.LatLng;
 
+private typedef OnePath = EitherType<Array<LatLngLiteral>, Array<LatLng>>;
+private typedef MultiPath = EitherType<Array<Array<LatLngLiteral>>, Array<Array<LatLng>>>;
+
 extern class Polygon extends MVCObject {
-	function new(options:{});
-	function getPaths():MVCArray<Path>;
-	function setPaths(v:EitherType<Array<Array<LatLngLiteral>>, Array<Array<LatLng>>>):Void;
+	function new(?options:{});
+	function setDraggable(v:Bool):Void;
+	function setEditable(v:Bool):Void;
 	function setMap(map:google.maps.Map):Void;
+	function setOptions(options:{}):Void;
+	function setPath(v:OnePath):Void;
+	function setPaths(v:EitherType<OnePath, MultiPath>):Void;
+	function setVisible(v:Bool):Void;
+	
+	
+	function getPaths():MVCArray<Path>;
 }
