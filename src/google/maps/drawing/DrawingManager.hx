16e95c39ccdfc83eb784aaf16456acecc60342f9
Kevin Leung <kevinresol@gmail.com>
2018-07-18 17:13:13 +0800

    Update DrawingManager

--- src/google/maps/drawing/DrawingManager.hx
+++ src/google/maps/drawing/DrawingManager.hx
@@ -4,7 +4,11 @@ import haxe.Constraints;
 
 extern class DrawingManager extends MVCObject {
 	function new(?options:{});
+	function getDrawingMode():OverlayType;
+	function getMap():google.maps.Map;
+	
 	function setDrawingMode(v:OverlayType):Void;
 	function setMap(map:google.maps.Map):Void;
 	function setOptions(options:{}):Void;
+	
 }
