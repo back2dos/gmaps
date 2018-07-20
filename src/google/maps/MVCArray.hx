3919b4114e96260c7cea592efc601e8a20717098
Kevin Leung <kevinresol@gmail.com>
2018-07-20 19:55:16 +0800

    Update MouseEvent and MVCArray

--- src/google/maps/MVCArray.hx
+++ src/google/maps/MVCArray.hx
@@ -1,6 +1,14 @@
 package google.maps;
 
 extern class MVCArray<T> extends MVCObject {
+	function clear():Void;
 	function forEach(f:T->Int->Void):Void;
 	function getArray():Array<T>;
+	function getAt(i:Int):T;
+	function getLength():Int;
+	function insertAt(i:Int, e:T):Void;
+	function pop():T;
+	function push(e:T):Int;
+	function removeAt(i:Int):T;
+	function setAt(i:Int, e:T):Void;
 }
--- src/google/maps/MouseEvent.hx
+++ src/google/maps/MouseEvent.hx
@@ -2,5 +2,7 @@ package google.maps;
 
 extern class MouseEvent  {
 	var latLng:LatLng;
+	var path:Int;
+	var vertex:Int;
 	function stop():Void;
 }
