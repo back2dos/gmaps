ac6cbab488b53e7f64b59aa9d10ee5ededf7a2f1
Kevin Leung <kevinresol@gmail.com>
2018-07-21 22:46:15 +0800

    Make LatLngLiteral an abstract

--- src/google/maps/LatLng.hx
+++ src/google/maps/LatLng.hx
@@ -5,7 +5,15 @@ extern class LatLng {
 	function lng():Float;
 }
 
-typedef LatLngLiteral = {
+@:forward
+abstract LatLngLiteral(Literal) from Literal to Literal {
+	@:from
+	public static inline function fromLatLng(v:LatLng):LatLngLiteral {
+		return {lat: v.lat(), lng: v.lng()}
+	}
+}
+
+private typedef Literal = {
 	var lat(default, never):Float;
 	var lng(default, never):Float;
 }
